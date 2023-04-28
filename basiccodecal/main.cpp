4082c5f7617468859fb340258719a44f5a7153c2
MD.Sakib Naim <98878762+SakibNaim@users.noreply.github.com>
2023-04-28 15:39:08 +0600

    Add files via upload
    basiccodepreview

---
+++ basiccodecal/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*creating same calculator extention*/
+    int a ,b,sum,sub;
+    cout<<"typa your firts valur:";
+    cin>>a;
+    cout<<"typa your second value:";
+    cin>>b;
+    sum = a + b;
+    sub = a - b;
+    cout<< "your total value "<<sum<<".";
+    cout<<"\nyour total value "<<sub<<".";
+    return 0;
+
+}
